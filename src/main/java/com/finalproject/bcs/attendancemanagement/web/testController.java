d5c6aa337a7030204cb6478c9b7c4d7e9ed65480
Muhammad Imran <khan.mine0071@gmail.com>
2021-03-13 12:34:25 +0100

    first working version

--- src/main/java/com/finalproject/bcs/attendancemanagement/web/testController.java
+++ src/main/java/com/finalproject/bcs/attendancemanagement/web/testController.java
@@ -3,7 +3,7 @@ package com.finalproject.bcs.attendancemanagement.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
 public class testController {
 
     @GetMapping("/index")
